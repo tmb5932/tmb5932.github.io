14a57b42bc0a467d0bf7d763ab0908076430468e
tmb5932 <tmb5932@g.rit.edu>
2024-09-07 21:28:29 -0400

    got rid of the js because I no longer have the flip effect

--- script.js
+++
@@ -1,9 +0,0 @@
-const projectBox = document.getElementById('pb-1');
-let isFlipped = false;
-
-projectBox.addEventListener('click', () => {
-  isFlipped = !isFlipped;
-  projectBox.style.transition = 'transform 0.6s';
-  projectBox.style.transformStyle = 'preserve-3d';
-  projectBox.style.transform = isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)';
-});
