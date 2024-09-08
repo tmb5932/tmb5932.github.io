1fe0f8ca9e6ea7b60f5552df38a40a3bd835d996
tmb5932 <tmb5932@g.rit.edu>
2024-09-08 18:55:43 -0400

    Added smooth navigation from the navbar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* External Links for Contact Section */
 document.getElementById("github-contact")
 .addEventListener("click", function () {
     window.open("https://github.com/tmb5932", '_blank');
@@ -11,4 +12,20 @@ document.getElementById("linkedin-contact")
 document.getElementById("resume-contact")
 .addEventListener("click", function () {
     window.open("assets/TB_Resume.pdf", '_blank');
+});
+
+/* Internal links to move between sections */
+document.getElementById("home-nav").addEventListener("click", function () {
+  const section = document.getElementById("home");
+  section.scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("project-nav").addEventListener("click", function () {
+  const section = document.getElementById("projects");
+  section.scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("contact-nav").addEventListener("click", function () {
+  const section = document.getElementById("contact");
+  section.scrollIntoView({ behavior: "smooth" });
 });
