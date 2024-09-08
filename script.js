17e6f6fcfe419750ab38e6708c6992432a6cbf37
tmb5932 <tmb5932@g.rit.edu>
2024-09-08 18:42:40 -0400

    Made the buttons work, and added a few more comments

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("github-contact")
+.addEventListener("click", function () {
+    window.open("https://github.com/tmb5932", '_blank');
+});
+
+document.getElementById("linkedin-contact")
+.addEventListener("click", function () {
+    window.open("https://linkedin.com/in/tbrown5932", '_blank');
+});
+
+document.getElementById("resume-contact")
+.addEventListener("click", function () {
+    window.open("assets/TB_Resume.pdf", '_blank');
+});
